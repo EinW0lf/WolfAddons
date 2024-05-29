f353aa973a4e1419dc62423c9ad113c7e4be1649
EinWolf <benpeng1711@gmail.com>
2024-05-29 17:03:28 +0200

    0.2.5
    - Resolved the scoreboard issue that was causing crashes.
    - Corrected additional issues introduced in the previous fix.

--- WolfAddons/utils/getScoreboard.js
+++ WolfAddons/utils/getScoreboard.js
@@ -1,17 +1,21 @@
 export function getScoreboard() {
-	const scoreboard = [];
+	try {
+		const scoreboard = [];
 
-	const Lines = Scoreboard?.getLines();
-	if (!Lines || Lines.length >= 0) return null;
+		const Lines = Scoreboard?.getLines();
+		if (!Lines || Lines.length <= 0) return null;
 
-	Lines.forEach((element) => {
-		scoreboard.push(
-			element
-				.toString()
-				.replace(/§[0-9a-fk-or]/g, "")
-				.replace(/[^a-zA-Z0-9\s\u00C0-\u00FF,.:;_-]/g, "", "")
-				.trim()
-		);
-	});
-	return scoreboard;
+		Lines.forEach((element) => {
+			scoreboard.push(
+				element
+					.toString()
+					.replace(/§[0-9a-fk-or]/g, "")
+					.replace(/[^a-zA-Z0-9\s\u00C0-\u00FF,.:;_-]/g, "", "")
+					.trim()
+			);
+		});
+		return scoreboard;
+	} catch (error) {
+		return null;
+	}
 }
